7a941315edb6d8efdc42e0854eb82cebcbab7475
Anantkaushik99 <anantdevsingh24@gmail.com>
2026-05-27 08:45:45 +0530

    feat: JWT Auth - Register and Login APIs

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected!'))
   .catch((err) => console.log('MongoDB Error:', err))
 
+app.use('/api/auth', require('./routes/auth'))
 app.get('/', (req, res) => {
   res.json({ message: 'RentalHub API is running!' })
 })
